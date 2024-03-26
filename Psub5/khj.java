02cd25c0eb47723c8672b7d9e1ceb73246e7cdd9
HoJeongKimmm <2020111556@dgu.ac.kr>
2024-03-26 15:11:32 +0900

    1st commit

---
+++ Psub5/khj.java
@@ -0,0 +1,11 @@
+package Psub5;
+
+import java.util.Scanner;
+
+public class khj {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        System.out.println("이름을 입력하세요 : ", sc.next());
+		System.out.println("학번을 입력하세요 : ", sc.nextInt());
+	}
+}
